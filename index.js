e5929944898a786b996e5223a079989fede7af72
geomanuk20 <geomanuk20@gmail.com>
2025-03-03 20:34:07 +0530

    first

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://g-chat-backend-co5n.onrender.com",
     credentials: true,
   })
 );
